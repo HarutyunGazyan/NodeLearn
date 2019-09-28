dca0d821e4525d80ddee007301567c1cc3f76e98
harutyun.gazyan@gmail.com <harutyun.gazyan@gmail.com>
2019-09-28 19:01:17 +0400

    First test commit.

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,5 @@ import { Request, Response, NextFunction} from 'express';
 Init.registerGetRoute('/', ((req: Request, res: Response, next: NextFunction) => {
 	res.send('YO 2!');
 }))
-Init.registerGetRoute('/no', ((req: Request, res: Response, next: NextFunction) => {
-	res.send('no');
-}))
 
 Init.listen(5000);
